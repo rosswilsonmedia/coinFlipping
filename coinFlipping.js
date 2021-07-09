436ae9008821b97d6c9c6659a6a4ec1708224fa7
rosswilsonmedia <82285604+rosswilsonmedia@users.noreply.github.com>
2021-07-09 11:15:59 -0700

    coinFlipping commit

---
+++ coinFlipping.js
@@ -0,0 +1,47 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+// function fiveHeadsSync() {
+//     let headsCount = 0;
+//     let attempts = 0;
+//     while(headsCount < 5 && attempts<100) {
+//         attempts++;
+//         let result = tossCoin();
+//         console.log(`${result} was flipped`);
+//         if(result === "heads") {
+//             headsCount++;
+//         } else {
+//             headsCount = 0;
+//         }
+//     }
+//     return `It took ${attempts} tries to flip five "heads"`;
+// }
+// console.log( fiveHeadsSync() );
+// console.log( "This is run after the fiveHeadsSync function completes" );
+
+const fiveHeads = () => {
+    return new Promise( (resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+        while(headsCount < 5 && attempts<100) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if(headsCount>=5){
+            resolve(`Heads count was ${headsCount} in ${attempts} attempts`);
+        } else {
+            reject('Attempts reached 100 without getting heads 5 times');
+        }
+    });
+}
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+console.log( "When does this run now?" );
